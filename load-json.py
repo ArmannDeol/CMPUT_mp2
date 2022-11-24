76ba994e14176160fefe5e26950851017283d849
Raffle703 <rahultabraham703@gmail.com>
2022-11-24 02:52:55 -0700

    Add mongoimport command, and indexes

--- load-json.py
+++ load-json.py
@@ -1,5 +1,7 @@
 from sys import argv
+from os import system
 from pymongo import MongoClient, TEXT, ASCENDING, DESCENDING
+from subprocess import run, DEVNULL
 import json
 
 
@@ -20,33 +22,70 @@ def mongoimport(jsonfile, db_name, coll_name, db_port):
     client = MongoClient(port_connection)
     db = client[db_name]
     coll = db[coll_name]
-    coll.drop()
-    print('Dropping table...')
-
-    count = 0
-    with open(jsonfile) as file:
-        line = file.readline()
-        print("Running...")
+    run(['mongoimport', '-d', db_name, '-c', coll_name, '--file', jsonfile, '--port', db_port, '--drop'])
+    # TODO DELETE
+    # count = 0
+    # with open(jsonfile) as file:
+    #     line = file.readline()
+    #     # print("Running...")
         
-        while line:
-            line = json.loads(line)
-            coll.insert_one(line)
-            line = file.readline()
-            count += 1
-    
-    addIndex = [('title', TEXT), ('year', TEXT), ('abstract', TEXT), ('venue', TEXT), ('authors', TEXT)]
-    print('Loaded ' + str(count) + ' entries...')
-    print('Creating Indexes...')
-    coll.create_index(addIndex)      
+    #     while line:
+    #         line = json.loads(line)
+    #         coll.insert_one(line)
+    #         line = file.readline()
+    #         count += 1
+    # materialized view with venues and number of articles in venue
+    # matViewAgg = [
+    #     # need venue, num, and id
+    #     {
+    #         '$group' : {'_id' : '$venue', 'num_articles_in_venue' : {"$sum" : 1}}
+    #     },
+    #     {
+    #         '$project' : {'venue' : '$_id', '_id' : 0, 'num_articles_in_venue' : 1, 'id' : 1}
+    #     },
+    #     {
+    #         '$merge' : {'into': 'venueInfo'}
+    #     }
+    # ]
+    collVenues = db['venueInfo']
+    collVenues.drop()
+    matViewAgg = [
+        {
+            '$project' : {'venue' : 1, 'id' : 1}
+        },
+        {
+            '$merge' : {'into': 'venueInfo'}
+        }
+    ]
+    coll.aggregate(matViewAgg)
+    # TODO DELETE
+    matches = collVenues.find()
+    # for e in matches:
+    #     print(e)
+    # addIndex = [('title', TEXT), ('year', TEXT), ('abstract', TEXT), ('venue', TEXT), ('authors', TEXT)]
+    addIndex = [
+        ('abstract', TEXT),
+        ('authors', TEXT),
+        ('title', TEXT),
+        ('venue', TEXT),
+        ('year', TEXT)
+    ]
+    addRefIndex = [('references', DESCENDING)]
+    # print('Creating Indexes...')
+    coll.create_index(addIndex)
+    coll.create_index(addRefIndex)
+    # matches = coll.list_indexes()
+    # for e in matches:
+    #     print(e)
     return 
 
 def main():
-    if (len(argv)) > 1:
+    if (len(argv)) > 2:
         db_port = str(argv[1])
         jsonfile = str(argv[2])
     else:
         db_port = input('Database port: ')
-        #jsonfile = input('Enter json file name: ')
+        jsonfile = input('Enter json file name: ')
     # jsonfile = "dblp-ref-1m.json"
     # jsonfile = "dblp-ref-1k.json"
     # jsonfile = "dblp-ref-10.json"
