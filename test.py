bf8531d4eb2bc4fe283b77babce6a1536d8e12b6
Raffle703 <rahultabraham703@gmail.com>
2022-11-24 02:53:15 -0700

    Add more tests

--- test.py
+++ test.py
@@ -11,10 +11,10 @@ from unittest.mock import patch
 
 
 class TestName(unittest.TestCase):
-    def setUp(self):
-        port = '2000'
-        run(['python3', 'load-json.py', port, 'dblp-ref-10.json'], stdout=DEVNULL)
-        self.db = ds.connection(port)
+    # def setUp(self):
+    #     port = '2000'
+    #     run(['python3', 'load-json.py', port, 'dblp-ref-10.json'])
+    #     self.db = ds.connection(port)
 
     # @patch('document_store.input', create=True)
     # def test_addArticle_non_uniqueID(self, mock_input):
@@ -22,11 +22,29 @@ class TestName(unittest.TestCase):
     #     mock_input.side_effect = ['0040b022-1472-4f70-a753-74832df65266', '50']
     #     ds.addArticle(self.db)
         
+    # @patch('document_store.input', create=True)
+    # def test_addArticle_uniqueID(self, mock_input):
+    #     port = '2000'
+    #     run(['python3', 'load-json.py', port, 'dblp-ref-10.json'])
+    #     self.db = ds.connection(port)
+    #     id = '50'
+    #     # imitates user input
+    #     mock_input.side_effect = [id, 'FunnyTitle', 'Joe', 'Moe', '-1', '2000']
+    #     ds.addArticle(self.db)
+    #     coll = self.db['dblp']
+    #     matches = coll.find_one({'id' : id})
+    #     self.assertIsNotNone(matches)
+    
     @patch('document_store.input', create=True)
-    def test_addArticle_uniqueID(self, mock_input):
+    def test_listVenues(self, mock_input):
+        port = '2000'
+        run(['python3', 'load-json.py', port, 'dblp-test.json'])
+        self.db = ds.connection(port)
+        
+        n = 3
         # imitates user input
-        mock_input.side_effect = ['50', 'FunnyTitle', 'Joe', 'Moe', '-1', '2000']
-        ds.addArticle(self.db)
+        mock_input.side_effect = [n]
+        ds.listVenues(self.db)
 
 if __name__ == '__main__':
     unittest.main()
